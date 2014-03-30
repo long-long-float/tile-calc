22623a96871bfcc50269e78f6d530b0739b7d735
long_long_float <niinikazuki@yahoo.co.jp>
2014-03-30 11:51:10 +0900

    close app when app boot

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
 
 // 空のアプリケーション テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234227 を参照してください
 
@@ -31,79 +18,16 @@ namespace tile_calc
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
-        }
 
-        /// <summary>
-        /// アプリケーションがエンド ユーザーによって正常に起動されたときに呼び出されます。他のエントリ ポイントは、
-        /// アプリケーションが特定のファイルを開くために呼び出されたときなどに使用されます。
-        /// </summary>
-        /// <param name="e">起動要求とプロセスの詳細を表示します。</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
-        {
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            toastXml.GetElementsByTagName("text")[0].AppendChild(toastXml.CreateTextNode("Hello from toast!"));
 
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
+            var toast = new ToastNotification(toastXml);
+            toast.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(10);
 
-            Frame rootFrame = Window.Current.Content as Frame;
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
 
-            // ウィンドウに既にコンテンツが表示されている場合は、アプリケーションの初期化を繰り返さずに、
-            // ウィンドウがアクティブであることだけを確認してください
-            if (rootFrame == null)
-            {
-                // ナビゲーション コンテキストとして動作するフレームを作成し、最初のページに移動します
-                rootFrame = new Frame();
-                // 既定の言語を設定します
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: 以前中断したアプリケーションから状態を読み込みます。
-                }
-
-                // フレームを現在のウィンドウに配置します
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // ナビゲーションの履歴スタックが復元されていない場合、最初のページに移動します。
-                // このとき、必要な情報をナビゲーション パラメーターとして渡して、新しいページを
-                // 作成します
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-            // 現在のウィンドウがアクティブであることを確認します
-            Window.Current.Activate();
-        }
-
-        /// <summary>
-        /// 特定のページへの移動が失敗したときに呼び出されます
-        /// </summary>
-        /// <param name="sender">移動に失敗したフレーム</param>
-        /// <param name="e">ナビゲーション エラーの詳細</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// アプリケーションの実行が中断されたときに呼び出されます。アプリケーションの状態は、
-        /// アプリケーションが終了されるのか、メモリの内容がそのままで再開されるのか
-        /// わからない状態で保存されます。
-        /// </summary>
-        /// <param name="sender">中断要求の送信元。</param>
-        /// <param name="e">中断要求の詳細。</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: アプリケーションの状態を保存してバックグラウンドの動作があれば停止します
-            deferral.Complete();
+            Application.Current.Exit();
         }
     }
 }
