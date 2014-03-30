7b3504a5ca77ed82dbf0cc0c2db5121799fbf7b2
long_long_float <niinikazuki@yahoo.co.jp>
2014-03-31 00:11:41 +0900

    create calc buttons

--- tile-calc/button/MainPage.xaml.cs
+++ tile-calc/button/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.StartScreen;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,33 @@ namespace button
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            foreach(var i in Enumerable.Range(0, 1))
+            {
+                var secondaryTile = new SecondaryTile("calc-child" + i, i.ToString(), i.ToString(), new Uri("ms-appx:///Assets/Logo.scale-100.png"), TileSize.Square150x150);
+                await secondaryTile.RequestCreateAsync();
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var arg = (string)e.Parameter;
+
+            if (arg != "")
+            {
+                var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+                toastXml.GetElementsByTagName("text")[0].AppendChild(toastXml.CreateTextNode("clicked " + arg));
+
+                var toast = new ToastNotification(toastXml);
+                toast.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(10);
+
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+                Application.Current.Exit();
+            }
         }
     }
 }
