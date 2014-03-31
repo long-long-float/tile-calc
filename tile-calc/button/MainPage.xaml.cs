62375292f40ec447c629e5b1ded07c7c44c0182f
long_long_float <niinikazuki@yahoo.co.jp>
2014-03-31 21:18:41 +0900

    show pushed button

--- tile-calc/button/MainPage.xaml.cs
+++ tile-calc/button/MainPage.xaml.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Notifications;
+using Windows.UI.Popups;
 using Windows.UI.StartScreen;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -19,6 +23,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace button
 {
+
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
@@ -31,11 +36,22 @@ namespace button
 
         private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            foreach(var i in Enumerable.Range(0, 1))
+            foreach (var i in Enumerable.Range(0, 1))
             {
                 var secondaryTile = new SecondaryTile("calc-child" + i, i.ToString(), i.ToString(), new Uri("ms-appx:///Assets/Logo.scale-100.png"), TileSize.Square150x150);
                 await secondaryTile.RequestCreateAsync();
             }
+        }
+
+        private void showToast(String msg)
+        {
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            toastXml.GetElementsByTagName("text")[0].AppendChild(toastXml.CreateTextNode(msg));
+
+            var toast = new ToastNotification(toastXml);
+            toast.ExpirationTime = DateTimeOffset.UtcNow.AddSeconds(10);
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
@@ -44,7 +60,40 @@ namespace button
 
             if (arg != "")
             {
-                await Windows.System.Launcher.LaunchUriAsync(new Uri("tile-calc://" + arg));
+                var folder = ApplicationData.Current.LocalFolder;
+                var file = await folder.CreateFileAsync("state.json", Windows.Storage.CreationCollisionOption.OpenIfExists);
+                
+                string content;
+                using (var stream = (await file.OpenAsync(Windows.Storage.FileAccessMode.Read)).AsStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    content = await reader.ReadToEndAsync();
+                }
+
+                List<String> items;
+                try
+                {
+                    items = JsonArray.Parse(content)
+                        .Select(item => item.GetString())
+                        .ToList();
+                }
+                catch (System.Exception) //json parse error
+                {
+                    items = new List<String>();
+                }
+
+                items.Add(arg);
+
+                showToast(String.Join(", ", items));
+
+                JsonArray jsonAry = new JsonArray();
+                foreach (var item in items) jsonAry.Add(JsonValue.CreateStringValue(item));
+
+                using (var stream = (await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite)).AsStream())
+                using(var writer = new StreamWriter(stream))
+                {
+                    await writer.WriteAsync(jsonAry.Stringify());
+                }
 
                 Application.Current.Exit();
             }
